93a5c04e24b11890a7578bd619178100c375d6bb
karishkalyan <35295068+karishkalyan@users.noreply.github.com>
2018-02-19 16:26:36 +0530

    same order
    prgm

---
+++ prgm51.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+void main()
+{
+int num,digit,i,k,p[50];
+printf("\n enter the number:");
+scanf("%d",&num);
+while(num!=0)
+{
+digit=num%10;
+p[k]=digit;
+k++;
+num=num/10;
+}
+for(i=k-1;i=0;i--)
+{
+printf("%d\t",p[i]);
+}
+getch();
+}
+
+
+
